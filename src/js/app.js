f386879f89d8f5358c15f9fb444139e63b8e83ee
ngeorgiev <ngeorgiev.code@gmail.com>
2023-02-10 01:09:31 +0200

    3

--- src/js/app.js
+++ src/js/app.js
@@ -2,13 +2,13 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-  var productDiv = document.querySelector('.product');
-  var productPrice = document.querySelectorAll('p')[1].innerHTML;
-  var span = document.createElement('span');
-  span.class = "data-price";
-  span.className = "data-price";
-  span.innerText = productPrice;
-  productDiv.setAttribute('data-price', productPrice);
-  productDiv.appendChild(span);
+  const products = document.getElementsByClassName("product");
+const data = document.getElementsByClassName("price");
+  for (let i = 0; i < products.length; i++) {
+    products[i].setAttribute("data-price", data[i].textContent)
+    console.log(products[i]);
+    console.log(data[i]);
+    
+  }
 
 });
