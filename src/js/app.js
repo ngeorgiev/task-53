04dd933418d4faddc75bb339d9db00b07d46d10d
ngeorgiev <ngeorgiev.code@gmail.com>
2023-02-10 01:02:37 +0200

    2

--- src/js/app.js
+++ src/js/app.js
@@ -2,11 +2,13 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-  const products = document.querySelectorAll('.product');
-  products.forEach(product => {
-      const priceSpan = product.querySelector('.price');
-      const price = priceSpan.textContent;
-      product.setAttribute('data-price', price);
-  }) 
+  var productDiv = document.querySelector('.product');
+  var productPrice = document.querySelectorAll('p')[1].innerHTML;
+  var span = document.createElement('span');
+  span.class = "data-price";
+  span.className = "data-price";
+  span.innerText = productPrice;
+  productDiv.setAttribute('data-price', productPrice);
+  productDiv.appendChild(span);
 
 });
